93dbbf12006f182528e5aa664c8346b069714bb3
Abdelrahman_Abass <abdoabass400@gmail.com>
2023-10-17 19:27:09 +0300

    create spinner component

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import Navbar from "./Navbar";
 import GoogleAuth from "./GoogleAuth";
 import PrivateRoute from "./privateRoute";
+import Spinner from "./Spinner";
 export{
     Navbar,
     GoogleAuth,
     PrivateRoute,
+    Spinner,
 }
